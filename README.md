7fe047356f2a3c3295a2c9e36afd4429c3c8d087
Eirik Falck <eirik.falck@gmail.com>
2018-05-28 18:48:38 +0200

    changed code to stop for .5secs in middle each pass to more easily stop.

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 This is the code for the window wipers for DNV GL FuelFighter 2018 team. 
 
 It simply works by turning an Arduino on and running the code untill the power is cut and the wipers stop wiping. That's it. 
+
+
+(Image of pin mappnigs courtesy of Arudiono. [link](https://www.arduino.cc/en/Hacking/PinMapping))
--- window_wiper2018.ino
+++ window_wiper2018.ino
@@ -4,60 +4,48 @@
 #include <util/delay.h>
 
 #define PWM_TOP 6061
-#define SERVO_MIDDLE_US 1500e-6
-#define SERVO_MAX_US    2200e-6
-#define SERVO_MIN_US    800e-6
-#define FREQ 			330
+#define SERVO_MIDDLE_us 1500e-6
+#define SERVO_MAX_us    1800e-6  // abs max is 2200e-6
+#define SERVO_MIN_us    1200e-6  // abs min is  800e-6
+#define FREQ            330
+#define PIN_RESET_bm    (1<<PB4)  // is pin PB4 / digitalPin12
 
 // THIS CODE IS FOR THE SERVO MOTOR
 // POWER HD-1235MG
 
-int angleToUS(int angle) {
-	// returns us
-	return (int)(SERVO_MIN_US + (angle / 180.0) * (SERVO_MAX_US - SERVO_MIN_US));
-}
-
-void servoWrite(int us) {
-	OCR1A = ICR1 * us * FREQ;
-}
-
-void servoWriteAngle(int angle) {
-	int us = angleToUS(angle);
-	servoWrite(us);
-}
-
 int main() {
-	DDRB |= (1<<PB1);
-
-	// set top to 6061 <=> 330 Hz
-	ICR1 = PWM_TOP;
-
-	// 90 deg  -  step 3000
-	OCR1A = ICR1 * (SERVO_MIDDLE_US * FREQ);
-
-	TCCR1A |= (1<<WGM11) | (1<<COM1A1);
-	TCCR1B |= (1<<WGM13) | (1<<WGM12);
-	TCCR1B |= (1<<CS11);   // 8 prescalar
+    DDRB |= (1<<PB1);  // PWM for servo
+    DDRB &= ~PIN_RESET_bm;  // pin for reset set as input
 
 
-	int incr = 1;
-	int angle = 0;
+    // set top to 6061 <=> 330 Hz
+    ICR1 = PWM_TOP;
 
-	while (1) {
-		//OCR1A = (int)pos;
-		OCR1A += incr;
-		_delay_us(700);
-		// _delay_ms(1);
+    // 90 deg  -  step 3000
+    OCR1A = ICR1 * (SERVO_MIDDLE_us * FREQ);
 
-		if (OCR1A >= ICR1 * (1800e-6 * FREQ)) {
-			incr = -1;
-			_delay_ms(500);
-		}
-		else if (OCR1A <= ICR1 * (1200e-6 * FREQ)) {
-			incr = 1;
-			_delay_ms(500);
-		}
-	}
+    TCCR1A |= (1<<WGM11) | (1<<COM1A1);
+    TCCR1B |= (1<<WGM13) | (1<<WGM12);
+    TCCR1B |= (1<<CS11);   // 8 prescalar
 
-	return 0;
+
+    int incr = 1;
+    while (1) {
+        if (abs(OCR1A - ICR1 * (SERVO_MIDDLE_us * FREQ)) <= 4)  // is close to the middle
+            _delay_ms(500);
+
+        OCR1A += incr;
+        _delay_us(700);
+
+        if (OCR1A >= ICR1 * (SERVO_MAX_us * FREQ)) {
+            incr = -1;
+            _delay_ms(500);
+        }
+        else if (OCR1A <= ICR1 * (SERVO_MIN_us * FREQ)) {
+            incr = 1;
+            _delay_ms(500);
+        }
+    }
+
+    return 0;
 }
